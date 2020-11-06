9a1a72d2c9505e209f9b4308572aa038c938026a
GDCStoney <g.stoney@btinternet.com>
2020-11-06 10:24:21 +0000

    travis user postgres

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,7 +4,7 @@ require 'pg'
 
 def setup_test_database
   p 'Setting up test database ... '
-  connection = PG.connect(dbname: 'bookmark_manager_test')
+  connection = PG.connect(dbname: 'bookmark_manager_test', user: 'postgres')
 
   connection.exec('TRUNCATE bookmarks;')
 end
